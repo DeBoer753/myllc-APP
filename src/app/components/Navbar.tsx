582c09782fd917c336bb049c6a68af056719d28a
Myles DeBoer <mylesdeboer@Myless-MacBook-Pro-2.local>
2023-12-05 20:45:45 -0800

    netlify redploy after adding ? after childeren in nvabar href

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -38,7 +38,7 @@ const NavLink = (props: Props) => {
         textDecoration: 'none',
         bg: useColorModeValue('gray.200', 'gray.700')
       }}
-      href={`#${children.toLowerCase()}`}>
+      href={`#${children?.toLowerCase()}`}>
       {children}
     </Box>
   )
