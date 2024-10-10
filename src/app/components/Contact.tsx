ecd68159cda97a948c401156d3280ddd0597e6b6
Myles DeBoer <mylesdeboer@Myless-MacBook-Pro-2.local>
2024-10-10 09:38:35 -0700

    added contact background

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -23,14 +23,14 @@ export default function ContactFormWithSocialButtons() {
   const { hasCopied, onCopy } = useClipboard('example@example.com')
 
   return (
-    <Container maxW={'100%'} id="contact"  backgroundImage={`url('/banner5TemplateCBM.png')`} backgroundSize='cover' backgroundPosition="center" backgroundAttachment='fixed'>
+    <Container maxW={'100%'} id="contact"  backgroundImage={`url('/contact_bg.jpg')`} backgroundSize='cover' backgroundAttachment='fixed'>
       <Flex align="center" justify="center" flexDir={'column'} p={{ base: 16, lg: 16 }}>
 
       <Heading textAlign={'center'} mb={{lg: '50px', sm: '30px', base: '30px'}} fontSize={{ base: '4xl', md: '5xl',}}>
           Let&apos;s Chat
       </Heading>
 
-      <Text maxW={'2xl'} textAlign={'center'} mb={10}>
+      <Text maxW={'2xl'} textAlign={'center'} mb={10} p={2} fontSize={15} fontWeight={'bold'}>
           We invite you to embark on a journey of digital transformation by securing a tailor-made website for your business. Our dedicated team is eager to collaborate with you, crafting a digital experience that seamlessly aligns with your brand identity. Act now and be among our first three clients to enjoy exclusive savings, unlocking a world of possibilities at an unprecedented value. Don&apos;t miss out on this limited-time opportunity to elevate your online presence while making the most of our special discounts. Reach out today, and let&apos;s turn your web dreams into a reality! 
       </Text>
 
