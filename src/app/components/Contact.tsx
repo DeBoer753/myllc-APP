d9277819b3eff9064b2534186e0b665a0e17c091
deboer753 <myles.deboer@gmail.com>
2025-03-01 08:43:09 -0800

    adjusted testimonials

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -41,39 +41,36 @@ export default function Contact() {
   }
   
   return (
-< Container
-  maxW="100%"
-  id="contact"
-  backgroundImage={`url('/imgs/contact_banner.png')`}
-  sx={{
-    backgroundAttachment: 'scroll', 
-  
-    '@media screen and (min-width: 390px)': {
-      backgroundAttachment: 'scroll',
-      backgroundSize: '200% auto', 
+    < Container
+      maxW="100%"
+      id="contact"
+      backgroundImage={`url('/imgs/contact_banner.png')`}
+      sx={{
+        backgroundAttachment: 'scroll', 
+      
+        '@media screen and (min-width: 390px)': {
+          backgroundAttachment: 'scroll',
+          backgroundSize: '200% auto', 
 
- 
-    },
-    '@media screen and (min-width: 768px)': {
-      backgroundAttachment: 'scroll', 
+    
+        },
+        '@media screen and (min-width: 768px)': {
+          backgroundAttachment: 'scroll', 
 
-    },
-    '@media screen and (min-width: 1024px)': {
-      backgroundAttachment: 'fixed',
-      backgroundSize: '100% auto', 
-    },
-  }}
-> 
+        },
+        '@media screen and (min-width: 1024px)': {
+          backgroundAttachment: 'fixed',
+          backgroundSize: '100% auto', 
+        },
+      }}
+    >    
+
       
       <Flex align="center" justify="center" flexDir={'column'} p={{ base: 16, lg: 16 }}>
 
       <Heading color={'white'} textAlign={'center'} mb={{lg: '50px', sm: '30px', base: '30px'}} fontSize={{ base: '4xl', md: '5xl',}}>
           Let&apos;s Chat
       </Heading>
-
-      <Text ml={-10} mr={-10} maxW={'3xl'} textAlign={'center'} mb={10} fontSize={15} fontWeight={'bold'} color={'white'}>
-      We invite you to embark on a journey of digital transformation with a custom-designed website tailored to your business needs. Our experienced team is ready to collaborate with you, creating a digital presence that perfectly reflects your brand identity. As a special offer, the first three clients will receive exclusive savings, providing exceptional value while enhancing your online presence. Don’t miss this limited-time opportunity to elevate your brand with a professional, high-quality website. Contact us today, and let’s bring your vision to life. 
-      </Text>
           
         <Box pl={0} pb={{base: 10}}>
           <VStack spacing={{ base: 4, md: 8, lg: 20 }}>
